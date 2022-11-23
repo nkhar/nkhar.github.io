15b4cf02a934cd951f2cbc68c1635ba061ded893
nkhar iMac <nikoloz.kharshiladze.2014@live.rhul.ac.uk>
2022-11-23 18:34:54 +0400

    Added about.tsx

---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+import styles from "../styles/Home.module.css";
+
+export default function About() {
+  <h1 className={styles.title}>
+    Learn <a href="https://nextjs.org">Next.js!</a>
+  </h1>;
+}
