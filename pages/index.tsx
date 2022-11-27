f1fec94a1787f77bfa91eb444cb9d3c158b98f19
nkhar iMac <nikoloz.kharshiladze.2014@live.rhul.ac.uk>
2022-11-27 19:33:49 +0400

    cleaned up homepage and style

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,34 +19,14 @@ export default function Home() {
       </Head>
 
       <DashboardHeader />
-      <AboutUs />
-      <DoctorList />
-      <ServiceList />
-      <Schedule />
-      <MapFrame />
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-        </div>
+        <AboutUs />
+        <DoctorList />
+        <ServiceList />
+        <Schedule />
       </main>
+      <MapFrame />
 
       <Footer />
     </div>
