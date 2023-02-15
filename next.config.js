f4cdaa3de457ffe888384f9376022cf7d312c8c4
nkhar iMac <nikoloz.kharshiladze.2014@live.rhul.ac.uk>
2023-02-15 17:17:31 +0400

    corrected i18n in next.config.js

--- next.config.js
+++ next.config.js
@@ -6,19 +6,16 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
+
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ["en", "ka", "ru"],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "en",
+    localeDetection: false,
+  },
 };
 
-module.exports = [
-  nextConfig,
-  {
-    i18n: {
-      // These are all the locales you want to support in
-      // your application
-      locales: ["en", "ka", "ru"],
-      // This is the default locale you want to be used when visiting
-      // a non-locale prefixed path e.g. `/hello`
-      defaultLocale: "en",
-      localeDetection: false,
-    },
-  },
-];
+module.exports = nextConfig;
